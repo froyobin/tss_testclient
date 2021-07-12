6b613045b35bbfa872de834ba8e8299f5822f4e3
ford betelgeusedent <ford.betelgeusedent@gmail.com>
2021-07-13 09:41:38 +1000

    testing eddsa and ecdsa

--- tss_client.go
+++ tss_client.go
@@ -5,57 +5,20 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"gitlab.com/thorchain/tss/go-tss/keygen"
+	"gitlab.com/thorchain/tss/go-tss/keysign"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
-
-
 )
 
-
-type Status byte
-const ALGO="eddsa2"
-
-
-type Node struct {
-	Pubkey         string `json:"pubkey"`
-	BlameData      []byte `json:"data"`
-	BlameSignature []byte `json:"signature"`
-}
-
-// Blame is used to store the blame nodes and the fail reason
-type Blame struct {
-	FailReason string `json:"fail_reason"`
-	BlameNodes []Node `json:"blame_peers,omitempty"`
-}
-
-// Response keygen response
-type KeyGenResponse struct {
-	PubKey      string        `json:"pub_key"`
-	PoolAddress string        `json:"pool_address"`
-	Status      Status `json:"status"`
-	Blame       Blame   `json:"blame"`
-}
-
-type KeyGenRequest struct {
-	Keys []string `json:"keys"`
-	Algo          string   `json:"algo"`
-	
-}
-
-type KeySignRequest struct {
-	PoolPubKey    string   `json:"pool_pub_key"` // pub key of the pool that we would like to send this message from
-	Message       string   `json:"message"`      // base64 encoded message to be signed
-	SignerPubKeys []string `json:"signer_pub_keys"`
-		Algo          string   `json:"algo"`
-
-}
-func sendTestRequest(url string, request []byte) []byte {
+func sendTestRequest(url, algo string, request []byte) []byte {
 	var resp *http.Response
 	var err error
-	fmt.Println(url)
+	fmt.Printf("%v>>%v\n", algo, url)
 	if len(request) == 0 {
 		resp, err = http.Get(url)
 		if err != nil {
@@ -75,14 +38,48 @@ func sendTestRequest(url string, request []byte) []byte {
 	return body
 }
 
-
-func KeyGen2(testPubKeys []string, ip string, ports []int) string {
-	var keyGenRespArr []*KeyGenResponse
+func KeyGenAll(testPubKeys []string, ip string, ports []int, algo string) (string, string) {
+	var keyGenRespArr [][]keygen.Response
 	var locker sync.Mutex
-	keyGenReq := KeyGenRequest{
-		Keys: testPubKeys,
-		Algo:ALGO,
+	keyGenReq := keygen.NewRequest(testPubKeys, 10, "0.14.0", algo)
+	request, err := json.Marshal(keyGenReq)
+	if err != nil {
+		return "", ""
 	}
+
+	requestGroup := sync.WaitGroup{}
+	for i := 0; i < len(ports); i++ {
+		requestGroup.Add(1)
+		go func(i int, request []byte, locker *sync.Mutex) {
+			//sleepTime := time.Second * time.Duration(rand.Int()%5)
+			//fmt.Printf("we sleep -------%v\n", sleepTime)
+			//time.Sleep(sleepTime)
+			defer requestGroup.Done()
+			url := fmt.Sprintf("http://%s:%d/keygenall", ip, ports[i])
+			respByte := sendTestRequest(url, algo, request)
+			if respByte == nil {
+				fmt.Println("error in send keygen")
+				return
+			}
+			var tempResp []keygen.Response
+			err = json.Unmarshal(respByte, &tempResp)
+			if err != nil {
+				fmt.Println("error in unmarshal the data")
+			}
+			locker.Lock()
+			keyGenRespArr = append(keyGenRespArr, tempResp)
+			locker.Unlock()
+		}(i, request, &locker)
+
+	}
+	requestGroup.Wait()
+	return keyGenRespArr[0][0].PubKey, keyGenRespArr[0][1].PubKey
+}
+
+func KeyGen(testPubKeys []string, ip string, ports []int, algo string) string {
+	var keyGenRespArr []*keygen.Response
+	var locker sync.Mutex
+	keyGenReq := keygen.NewRequest(testPubKeys, 10, "0.14.0", algo)
 	request, err := json.Marshal(keyGenReq)
 	if err != nil {
 		return ""
@@ -91,80 +88,154 @@ func KeyGen2(testPubKeys []string, ip string, ports []int) string {
 	requestGroup := sync.WaitGroup{}
 	for i := 0; i < len(ports); i++ {
 		requestGroup.Add(1)
-		go func(i int, request []byte, keygenRespAddr *[]*KeyGenResponse, locker *sync.Mutex) {
+		go func(i int, request []byte, locker *sync.Mutex) {
 			//sleepTime := time.Second * time.Duration(rand.Int()%5)
 			//fmt.Printf("we sleep -------%v\n", sleepTime)
 			//time.Sleep(sleepTime)
 			defer requestGroup.Done()
 			url := fmt.Sprintf("http://%s:%d/keygen", ip, ports[i])
-			respByte := sendTestRequest(url, request)
-			if respByte ==nil{
+			respByte := sendTestRequest(url, algo, request)
+			if respByte == nil {
 				fmt.Println("error in send keygen")
 				return
 			}
-			var tempResp KeyGenResponse
+			var tempResp keygen.Response
 			err = json.Unmarshal(respByte, &tempResp)
 			if err != nil {
-				fmt.Printf("22222>>>>>err%v, and bytes=%v\n", err, respByte)
+				fmt.Println("error in unmarshal the data")
 			}
 			locker.Lock()
-			*keygenRespAddr = append(*keygenRespAddr, &tempResp)
+			keyGenRespArr = append(keyGenRespArr, &tempResp)
 			locker.Unlock()
-		}(i, request, &keyGenRespArr, &locker)
+		}(i, request, &locker)
 
 	}
 
 	requestGroup.Wait()
-
-	for i := 0; i < len(ports); i++ {
-		fmt.Printf("%d------%s\n", i, keyGenRespArr[i].PubKey)
-	}
 	return keyGenRespArr[0].PubKey
 }
 
+func KeySign2(poolPubKey, msg1Str, msg2Str string, ip string, blockHeight int64, ports []int, signersPubKey []string, algo string) {
 
+	msg1 := base64.StdEncoding.EncodeToString([]byte(msg1Str))
+	msg2 := base64.StdEncoding.EncodeToString([]byte(msg2Str))
 
-func KeySign2(poolPubKey string, ip string, ports []int, signersPubKey []string) {
-
-	msg := base64.StdEncoding.EncodeToString([]byte("hello"))
-
-	keySignReq := KeySignRequest{
-		PoolPubKey:    poolPubKey,
-		Message:       msg,
-		SignerPubKeys: signersPubKey,
-		Algo:ALGO,
-	}
+	keySignReq := keysign.NewRequest(poolPubKey, []string{msg1, msg2}, blockHeight, signersPubKey, "0.14.0", algo)
 	request, _ := json.Marshal(keySignReq)
+
+	keySignReq2 := keysign.NewRequest(poolPubKey, []string{msg1, msg2}, blockHeight, signersPubKey[1:], "0.14.0", algo)
+
+	request2, _ := json.Marshal(keySignReq2)
+	_ = request2
 	requestGroup := sync.WaitGroup{}
+	var result []keysign.Response
+	locker := sync.Mutex{}
 	for i := 0; i < len(ports); i++ {
 		requestGroup.Add(1)
-		go func(idx int, request []byte) {
+		go func(idx int, request []byte, locker *sync.Mutex) {
 			defer requestGroup.Done()
+			var respByte []byte
 			url := fmt.Sprintf("http://%s:%d/keysign", ip, ports[idx])
-			if idx != 2 {
-				time.Sleep(time.Second * time.Duration(rand.Int()%5))
+			//if idx != 2 {
+			//	time.Sleep(time.Second * time.Duration(rand.Int()%5))
+			//}
+			if idx == 1000000 {
+				respByte = sendTestRequest(url, algo, request)
+				if respByte == nil {
+					fmt.Println("error in send keysign")
+					return
+				}
+			} else {
+				respByte = sendTestRequest(url, algo, request)
+				if respByte == nil {
+					fmt.Println("error in send keysign")
+					return
+				}
 			}
-			respByte := sendTestRequest(url, request)
-			if respByte ==nil{
+
+			var ret keysign.Response
+			err := json.Unmarshal(respByte, &ret)
+			if err != nil {
 				fmt.Println("error in send keysign")
 				return
 			}
-			fmt.Printf("current time=%s::%d:--> %s\n",time.Now().Local(), idx, respByte)
-
-		}(i, request)
+			if ret.Status != 1 {
+				panic("error")
+			}
+			locker.Lock()
+			result = append(result, ret)
+			locker.Unlock()
+		}(i, request, &locker)
 	}
 	requestGroup.Wait()
 
+	if len(result) != 4 {
+		panic("error in keysign verification")
+	}
+
+	fmt.Printf("---------------\n")
+	for _, el := range result[0].Signatures {
+		fmt.Printf("%v-%v---s:%v----r:%v\n", algo, el.Msg, el.S, el.R)
+	}
+	fmt.Printf("---------------\n")
+
 }
 
+func doJob(job chan int, wg *sync.WaitGroup, ip string, algos, poolKeys, testPubKeys []string, ports []int) {
+	defer wg.Done()
+	for {
+		i, ok := <-job
+		if !ok {
+			return
+		}
 
-func main () {
+		choose := rand.Intn(2)
+		algo := algos[choose]
+		poolKey := poolKeys[choose]
+		msg1 := "he" + strconv.Itoa(i)
+		msg2 := "hee" + strconv.Itoa(i)
+		KeySign2(poolKey, msg1, msg2, ip, int64(i), ports, testPubKeys[:], algo)
+	}
+
+}
+
+func main() {
 	testPubKeys := []string{"thorpub1addwnpepqtdklw8tf3anjz7nn5fly3uvq2e67w2apn560s4smmrt9e3x52nt2svmmu3", "thorpub1addwnpepqtspqyy6gk22u37ztra4hq3hdakc0w0k60sfy849mlml2vrpfr0wvm6uz09", "thorpub1addwnpepq2ryyje5zr09lq7gqptjwnxqsy2vcdngvwd6z7yt5yjcnyj8c8cn559xe69", "thorpub1addwnpepqfjcw5l4ay5t00c32mmlky7qrppepxzdlkcwfs2fd5u73qrwna0vzag3y4j"}
 	ip := "127.0.0.1"
 	ports := []int{8320, 8321, 8322, 8323}
-	poolAddr := KeyGen2(testPubKeys, ip, ports)
-	fmt.Println(poolAddr)
-	for i := 0; i < 1; i++ {
-		KeySign2(poolAddr, ip, ports, testPubKeys[:3])
+	wg := &sync.WaitGroup{}
+	algos := []string{"ecdsa", "eddsa"}
+
+	//poolAddrEcdsa := KeyGen(testPubKeys, ip, ports, "ecdsa")
+	//pubkey1, pubkey2 := KeyGenAll(testPubKeys, ip, ports, "")
+	//fmt.Printf("ecdsa:%v\neddsa:%v\n", pubkey1, pubkey2)
+	//return
+	poolAddrEcdsa := "thorpub1addwnpepqdpk0vtztrwdy3la57p4facsvs4fk6l59dkecvsrgldy9lyaga5ugntqqlz"
+	poolAddrEddsa := "thorpub1zcjduepqhyy3gmt22uv3fnnnq9qls27f2suda3f68vasxlqa2cqzg8sag4qsmx26fm"
+	poolkeys := []string{poolAddrEcdsa, poolAddrEddsa}
+	_ = poolAddrEcdsa
+	_ = poolAddrEddsa
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(algos)
+
+	taskNum := 4
+	jobChan := make(chan int, taskNum)
+
+	base := 100
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := base; i < base+20; i++ {
+			jobChan <- i
+		}
+		close(jobChan)
+	}()
+
+	for i := 0; i < taskNum; i++ {
+		wg.Add(1)
+		go doJob(jobChan, wg, ip, algos, poolkeys, testPubKeys[:], ports)
 	}
+
+	wg.Wait()
+	fmt.Println("quit")
 }
